f32cacd141ffca1f245f308fcf8f60d3ef47a126
nutcasev15 <tab.andsupp@gmail.com>
2017-04-02 21:45:26 +0530

    Toolchain Iteration 6.3 with Custom GSR of Android 7.x.x Many thanks to @sayeed99 for the GSR Commited on 02/04/17 21:46

---
+++ lib/gcc/x86_64-linux-gnu/6.3.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --build=x86_64-linux-gnu --target=x86_64-linux-gnu --with-gcc-version=Linaro6.3SN1702 --with-cloog --with-isl --with-newlib --with-ppl --enable-shared --enable-gnu-indirect-function --disable-libcilkrts --disable-libsanitizer --disable-libssp --enable-libatomic-ifuncs=no --enable-languages=c,c++ --enable-graphite=yes --enable-plugins --enable-gold=default --disable-option-checking --enable-headers --disable-bootstrap --enable-threads --with-fpmath=sse --disable-nls --with-gnu-as --with-gnu-ld --enable-lto --enable-multiarch --with-diagnostics-color=always --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' --enable-eh-frame-hdr-for-static --enable-initfini-array --program-transform-name='s&^&x86_64-linux-&' --with-arch=x86-64 --with-tune=intel --enable-cloog-backend=isl --enable-multilib-list=m32,mx32,m64 --with-sysroot=/ --with-isysroot=/home/dev/Stuff/sys --with-python --prefix=/home/dev/Stuff/out";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" }, { "tune", "intel" } };
